a143a52c1654e993b7e67ceca01443c365a78d6b
Tasmiya-K-M <tasmiyakm2000@gmail.com>
2026-02-06 00:34:29 +0530

    changed

--- frontend.sh
+++ frontend.sh
@@ -1,15 +1,15 @@
 echo -e "\e[33mInstalling Nginx Server\e[0m"
-dnf install nginx -y > /tmp/roboshop.log
+dnf install nginx -y &>> /tmp/roboshop.log
 
 echo -e "\e[33mRemoving Old App content\e[0m"
-rm -rf /usr/share/nginx/html/* > /tmp/roboshop.log
+rm -rf /usr/share/nginx/html/* &>> /tmp/roboshop.log
 
 echo -e "\e[33mdownloading Frontend Content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip > /tmp/roboshop.log 2> /tmp/error.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip > /tmp/roboshop.log &>> /tmp/roboshop.log
 
 echo -e "\e[33mExtracting Frontend Content\e[0m"
 cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip > /tmp/roboshop.log
+unzip /tmp/frontend.zip &>> /tmp/roboshop.log
 
 # vim /etc/nginx/default.d/roboshop.conf 
 
