fff5089331688bbe6d552943dfc57675ef77eb03
Tasmiya-K-M <tasmiyakm2000@gmail.com>
2026-02-16 21:28:39 +0530

    modified

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,8 +2,8 @@ a=10
 echo $a
 echo ${a}
 
-# special variables are inputs $0 to $n, $*, $#
-echo $0
-echo $1
-echo $*
-echo $#
+# special variables are inputs $0 to $n, $*, $# (pass arguments when running the script)
+echo script name - $0 # print script name
+echo first argument - $1 # print first argument
+echo all arguments - $* # print all arguments
+echo number of arguemnts - $# # print number of arguemnts
