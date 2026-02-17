9fe4248b5163ad2a410048ba74fd40cc546d0c55
Tasmiya-K-M <tasmiyakm2000@gmail.com>
2026-02-17 16:03:35 +0530

    modified

--- 03-variables.sh
+++ 03-variables.sh
@@ -7,3 +7,9 @@ echo script name - $0 # print script name
 echo first argument - $1 # print first argument
 echo all arguments - $* # print all arguments
 echo number of arguemnts - $# # print number of arguemnts
+
+sum=$((1+1))
+echo $sum
+
+
+today_date=$(date)
