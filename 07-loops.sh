992929cf6e2cf783b5569c600b90484a39e26b13
Tasmiya-K-M <tasmiyakm2000@gmail.com>
2026-02-17 19:45:50 +0530

    modified

--- 07-loops.sh
+++ 07-loops.sh
@@ -1,14 +1,14 @@
 #if we want to repeat the same code multiple times, we will use loops concept
 
 a=10
-while [ $a -gt  0 ]; do  # if not sure about iteration go with while
+while [ $a -gt  0 ]; do  # if not sure about iteration go with while (work on expressions)
     echo hello world
     a=$(($a-1))
     sleep 1 # wait for 1sec
 done
 
 
-for component in catalogue user cart frontend shipping payment; do
+for component in catalogue user cart frontend shipping payment; do # when inputs are clear go with for (work on inputs)
     echo creating component - $component
     sleep 1
 done
