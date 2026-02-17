63e42e768afea3cf46b9bb778faff5fa4978f882
Tasmiya-K-M <tasmiyakm2000@gmail.com>
2026-02-17 22:57:50 +0530

    modified

--- mediawiki/apache.sh
+++ mediawiki/apache.sh
@@ -1,5 +1,15 @@
-cd /tmp
-url=https://releases.wikimedia.org/mediawiki/1.45/mediawiki-1.45.1.tar.gz
-curl -O $url
-tar -xf /tmp/mediawiki-1.45.1.tar.gz
-yum install httpd -y
+cd /tmp 
+url=https://releases.wikimedia.org/mediawiki/1.45/mediawiki-1.45.1.tar.gz 
+
+echo Downloaded the mediawiki tar file
+curl -O $url &>> /var/log/mediawiki.log
+
+echo unzip the tar file
+tar -xf /tmp/mediawiki-1.45.1.tar.gz &>> /var/log/mediawiki.log
+
+echo installing apache
+yum install httpd -y &>> /var/log/mediawiki.log
+
+mv mediawiki-1.45.1 mediawiki
+rm -rf /var/www/html/*
+cp mediawiki /var/www/html/
